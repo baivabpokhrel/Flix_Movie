d483c875d1db6c90516eb56ace861cf5512a1e0d
Binod Pokhrel <Binod@Binods-MacBook-Pro.local>
2018-10-01 16:31:20 -0400

    fanalized version

---
+++ Pods/Target Support Files/Pods-Flix-MovieTests/Pods-Flix-MovieTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flix_MovieTests : NSObject
+@end
+@implementation PodsDummy_Pods_Flix_MovieTests
+@end
---
+++ README.md
@@ -0,0 +1,2 @@
+# Flix Movie Feed
+This app displaya the movie from an API and displays in the form of table view.
